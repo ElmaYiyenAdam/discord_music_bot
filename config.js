9886617b1460c80d1d4a111c93e7dbd6168b2eb5
ElmaYiyenAdam <86776229+ElmaYiyenAdam@users.noreply.github.com>
2025-09-05 17:43:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://saygincamsoy2005_db_user:<db_password>@discordmusicbot.zvefwid.mongodb.net/?retryWrites=true&w=majority&appName=DiscordMusicBot",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
